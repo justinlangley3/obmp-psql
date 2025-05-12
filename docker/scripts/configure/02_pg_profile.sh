ee4ecd4b7e03f32228bf379edc881f38bfc5a061
Justin A Langley <justin.a.langley@gmail.com>
2025-05-12 12:50:42 +0000

    cleanup postgres env vars

--- docker/scripts/configure/02_pg_profile.sh
+++ docker/scripts/configure/02_pg_profile.sh
@@ -3,9 +3,9 @@ set -e
 
 echo "===> Configuring PostgreSQL Shell Profile"
 cat <<EOF > /usr/local/openbmp/pg_profile
-export PGUSER=$POSTGRES_USER
-export PGPASSWORD=$POSTGRES_PASSWORD
+export PGDATABASE=$POSTGRES_DB
 export PGHOST=$POSTGRES_HOST
 export PGPORT=$POSTGRES_PORT
-export PGDATABASE=$POSTGRES_DB
+export PGUSER=$POSTGRES_USERNAME
+export PGPASSWORD=$POSTGRES_PASSWORD
 EOF
